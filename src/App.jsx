eda26fa3e939148369141a543be45e1cc2b603d1
pranto113015 <pranto3876@gmail.com>
2025-06-30 01:09:04 +0600

    feat: update README and HTML title for Home Decoration Website; add home icon SVG

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/ite.svg'
 import './App.css'
 
 function App() {
   
+
   return (
     <>
       <p className='text-red-700 font-bold text-3xl'>This is my react and tailwind css work environment</p>
