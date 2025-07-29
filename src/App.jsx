d1bdb0265795893cad4e39dbe0c9d6f9aaf846c9
pranto113015 <pranto3876@gmail.com>
2025-07-29 13:54:00 +0600

    feat: implement routing with react-router and create page components for Home, About, Contact, and Shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-primary text-red-700 font-bold text-center p-20">
-        Hi, Developers!
-      </h1>
+      <Navbar />
+      <Outlet />
+      <footer>Footer</footer>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Shop from './pages/Shop.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact';
 
 // for react-router 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+
+
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <div><App /></div>
+    element: <App />,
+    children: [
+      {
+        path:'/',
+        element: <Home />
+      },
+      {
+        path:'/about',
+        element: <About />
+      },
+      {
+        path:'/contact',
+        element: <Contact />
+      },
+      {
+        path:'/shop',
+        element: <Shop />
+      }
+    ]
   }
 ]);
 
