2e5576712c24ee1b6ff3eda04758c8ff93123033
pranto113015 <pranto3876@gmail.com>
2025-07-30 23:54:13 +0600

    feat: update package.json and package-lock.json for Tailwind CSS integration; refactor Navbar component and clean up PostCSS configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,20 +1,5 @@
 export default {
   plugins: {
     "@tailwindcss/postcss": {},
-  },
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#E58441',
-        'secondary': '#1E1E1E',
-        'secondary-bg': '#F7F7F7'
-      },
-      fontFamily: {
-        'primary': ["Poppins", "sans-serif"]
-      }
-    },
-  },
+  }
 }
-
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,52 @@
-const Navbar =() =>{
-    return (
-        <div>navbar</div>
-    )
-}
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const navItems = () => [
+  { path: "/", label: "Furnitures" },
+  { path: "/shop", label: "Shop" },
+  { path: "/about", label: "About Us" },
+  { path: "/contact", label: "Contact" },
+];
+
+
+
+const NavItems = () => {
+  return (
+    <ul className="flex flex-col md:flex-row  items-center md:space-x-8 gap-8">
+      {
+        navItems().map((item, index) => (
+            <li key={index}>
+            <NavLink to={item.path} className={({isActive})=>isActive 
+            ? 'text-primary font-bold' 
+            : 'hover:text-primary'
+            }>{item.label}</NavLink>
+            </li>
+        ))
+      }
+    </ul>
+  );
+};
+
+
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="max-w-screen-2xl container mx-auto flex justify-between items-center py-6 px-4">
+
+        {/* logo */}
+        <Link to="/" className="font-bold">Logo</Link>
+
+        {/* menu items for desktop */}
+        <div>
+          <NavItems />
+        </div>
+
+        {/* cart items */}
+        <div>Cart items</div>
+
+      </nav>
+    </header>
+  );
+};
 export default Navbar;
