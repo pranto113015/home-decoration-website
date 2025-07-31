c77cf7adac84bbc23b5743b2db65e4d1f253f364
pranto113015 <pranto3876@gmail.com>
2025-07-31 13:24:01 +0600

    feat: add mobile menu close icon and improve NavItems component interaction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FaShoppingBag } from "react-icons/fa";
 import { FaBars } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 
 const navItems = () => [
   { path: "/", label: "Furnitures" },
@@ -12,12 +13,13 @@ const navItems = () => [
 
 
 
-const NavItems = () => {
+
+const NavItems = ({toggleMenu}) => {
   return (
     <ul className="flex flex-col md:flex-row  items-center md:space-x-8 gap-8">
       {
         navItems().map((item, index) => (
-            <li key={index}>
+            <li key={index} onClick={toggleMenu}>
             <NavLink to={item.path} className={({isActive})=>isActive 
             ? 'text-primary font-bold' 
             : 'hover:text-primary'
@@ -36,7 +38,7 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () =>{
-    setIsMenuOpen (prevState =>!prevState)
+    setIsMenuOpen (prev =>!prev);
   }
 
   return (
@@ -45,11 +47,6 @@ const Navbar = () => {
 
         {/* logo */}
         <Link to="/" className="font-bold">Logo</Link>
-
-        {/* menu items for desktop */}
-        <div className="hidden md:flex">
-          <NavItems />
-        </div>
 
 
 
@@ -60,6 +57,24 @@ const Navbar = () => {
           }
           
         </div>
+
+
+
+
+        {/* desktop menu items */}
+        <div className="hidden md:flex">
+          <NavItems />
+        </div>
+
+
+         {/* mobile menu items */}
+          <div className={`fixed top-0 left-0 w-full h-screen bg-black opacity-80 flex flex-col items-center justify-center space-y-8 text-white transition-transform transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} md:hidden`}>
+            <div className=" absolute top-5 right-5 cursor-pointer" onClick={toggleMenu}>
+              <FaTimes />
+            </div>
+            <NavItems  toggleMenu={toggleMenu}/>
+          </div>
+    
 
 
 
