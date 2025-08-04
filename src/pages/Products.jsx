edb85a9bbfc204e34a5654b58c99cd82e872e25a
pranto113015 <pranto3876@gmail.com>
2025-08-04 16:41:21 +0600

    add category tabs to Products component for better navigation

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 
+
 const Products = ({headline}) => {
+    const categories =["Chair","Beds","Sofa","Lamp"];
   return (
     <div>
       <div className='section-container'>
         <h2 className='text-4xl font-bold text-center my-8'>{headline}</h2>
+       
+       {/* category tabs */}
+
+       <div className='bg-[#EEEEEE] max-w-md mx-auto sm:rounded-full md:p-2 py-5 mb-16'>
+          <div className='flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4'>
+              {
+                categories.map((category)=>(
+                    <button key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-primary hover:text-white transition-colors`}>{category}</button>
+                ))
+            }
+          </div>
+       </div>
+
       </div>
     </div>
   )
 }
 
 export default Products
+
