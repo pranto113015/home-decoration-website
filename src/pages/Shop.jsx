f3cb0896c9ad7e9ea000e9a0087f21058b701a36
pranto113015 <pranto3876@gmail.com>
2025-08-04 16:03:10 +0600

    add Shop component with banner image and layout structure

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import bannerImg from '.././assets/banner.png'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <section className='min-h-screen'>
+      {/* banner */}
+      <div className='w-full h-[400px] bg-cover bg-center' style={{backgroundImage: `url(${bannerImg})`}}>
 
+
+      </div>
+
+    </section>
+
+    
+        
   )
 }
 
