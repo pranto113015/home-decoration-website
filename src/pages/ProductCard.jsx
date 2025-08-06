063854034fddd181ba03629377c35b52dab543b7
pranto113015 <pranto3876@gmail.com>
2025-08-06 22:37:08 +0600

    refactor ProductCard component to improve structure and readability

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -4,8 +4,15 @@ import { getImgUrl } from '../utils/getImageURL';
 const ProductCard = ({product}) => {
   return (
     <div>
+        {/* image fetch form products.js */}
       <div className='bg-[#FAFAFA]'>
         <img src={getImgUrl(`${product.imageUrl}`)} alt="product_img" />
+      </div>
+
+       {/* product card design */}
+      <div className='p-6 bg-white dark:bg-black shadow-sm'>
+        <h4 className='text-base mb-1'>{product.category}</h4>
+        <h3 className='font-semibold text-xl mb-2'>{product.name}</h3>
       </div>
     </div>
   )
