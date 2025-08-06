aab72c34cb85bc2b0d7e5bc313283f4ec97021b0
pranto113015 <pranto3876@gmail.com>
2025-08-06 23:34:09 +0600

    add Rating component to ProductCard for displaying product ratings

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getImgUrl } from '../utils/getImageURL';
+import Rating from './../components/Rating';
 
 const ProductCard = ({product}) => {
   return (
@@ -13,6 +14,7 @@ const ProductCard = ({product}) => {
       <div className='p-6 bg-white dark:bg-black shadow-sm'>
         <h4 className='text-base mb-1'>{product.category}</h4>
         <h3 className='font-semibold text-xl mb-2'>{product.name}</h3>
+        <Rating  rating={product.rating}/>
       </div>
     </div>
   )
