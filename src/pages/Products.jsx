741c8170a3555b3a697227466598892df5a1fc72
pranto113015 <pranto3876@gmail.com>
2025-08-06 22:31:14 +0600

    refactor Products component to apply grid layout to products display

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -24,7 +24,7 @@ const Products = ({headline}) => {
 
 
        {/* products grid */}
-       <div>
+       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6'>
         {
           products.map((product,index)=>(
             <div>
